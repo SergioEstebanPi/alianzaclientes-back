cb4a3d6405129be22a652906a3b48dde34364867
Sergio <sepi_147@hotmail.com>
2021-02-02 10:38:57 -0500

    Agrego configuración inicial del proyecto

---
+++ src/main/java/com/alianza/adminclientes/AlianzaclientesBackApplication.java
@@ -0,0 +1,13 @@
+package com.alianza.adminclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlianzaclientesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlianzaclientesBackApplication.class, args);
+	}
+
+}
